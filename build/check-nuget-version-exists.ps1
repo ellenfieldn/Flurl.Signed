8cebc503b8bc5e75944ddac111d14bb9f7dbd25a
Nathan Ellenfield <nathan.ellenfield@gmail.com>
2018-06-23 12:58:13 -0400

    Update powershell script to work

--- build/check-nuget-version-exists.ps1
+++ build/check-nuget-version-exists.ps1
@@ -1,14 +1,32 @@
 param (
   [string]$package,
-  [string]$version
+  [string]$version,
+  [string]$nugetIndexUrl = "https://api.nuget.org/v3/index.json"
 )
 
-try {
-  $url = "http://nuget.org/packages/$package/$version"
-  $status = (Invoke-WebRequest $url).StatusCode
-} catch {
-  $status = $_.Exception.Response.StatusCode.Value__
-}
+try 
+{
+  Write-Verbose "package: $package"
+  Write-Verbose "version: $version"
 
-$shouldPublishToNuGet = $status -eq '404'
-Write-Host $shouldPublishToNuGet
+  $indexResults = (Invoke-RestMethod $nugetIndexUrl)
+  Write-Verbose $indexResults
+  $packageResource = $indexResults.resources | Where { $_."@type" -eq "PackageBaseAddress/3.0.0" }
+  Write-Verbose $packageResource
+  $packageBaseUrl = $packageResource[0]."@id"
+  Write-Verbose $packageBaseUrl
+
+  $url = "$($packageBaseUrl)$($package)/index.json"
+  Write-Verbose $url
+  $packageJson = Invoke-RestMethod $url
+  Write-Verbose "Package: $packageJson"
+  
+  $hasVersion = $packageJson.versions.Contains($version)
+  Write-Verbose "HasVersion: $hasVersion"
+  Write-Host $hasVersion
+} 
+catch 
+{
+  Write-Error = $_.Exception.Response.StatusCode.Value__
+  Write-Host "False"
+}
