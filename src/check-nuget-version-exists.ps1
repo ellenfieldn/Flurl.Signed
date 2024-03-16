775ae896636589b5ae73f41f766187c9014d1ec1
Nathan Ellenfield <nathan.ellenfield@allscripts.com>
2024-03-16 11:49:52 -0400

    Fix package version check

--- src/check-nuget-version-exists.ps1
+++ src/check-nuget-version-exists.ps1
@@ -6,20 +6,21 @@ param (
 
 try 
 {
-  Write-Verbose "package: $package"
-  Write-Verbose "version: $version"
+  Write-Information "Package Name: $package"
+  Write-Information "Package Version: $version"
+  Write-Information "NuGet Index URL: $nugetIndexUrl"
 
+  Write-Information "Getting nuget index from $($nugetIndexUrl)..."
   $indexResults = (Invoke-RestMethod $nugetIndexUrl)
   Write-Verbose $indexResults
-  $packageResource = $indexResults.resources | Where { $_."@type" -eq "PackageBaseAddress/3.0.0" }
+  $packageResource = $indexResults.resources | Where-Object { $_."@type" -eq "PackageBaseAddress/3.0.0" }
   Write-Verbose $packageResource
   $packageBaseUrl = $packageResource[0]."@id"
   Write-Verbose $packageBaseUrl
 
-  $url = "$($packageBaseUrl)$($package)/index.json"
-  Write-Verbose $url
+  $url = "$($packageBaseUrl)$($package.ToLower())/index.json"
+  Write-Information "Getting list of package versions from $($url)"
   $packageJson = Invoke-RestMethod $url
-  Write-Verbose "Package: $packageJson"
   
   $hasVersion = $packageJson.versions.Contains($version)
   Write-Verbose "HasVersion: $hasVersion"
@@ -27,6 +28,6 @@ try
 } 
 catch 
 {
-  #Write-Error = $_.Exception
+  Write-Error = $_.Exception
   Write-Host "True"
 }
